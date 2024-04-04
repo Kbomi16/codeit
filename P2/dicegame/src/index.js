39a2bd96824a5d89345d689ddf1454308ff1d4ac
김보미 <kimbomi2172@naver.com>
2024-04-04 13:43:15 +0900

    ✅ dicegame 실습1

--- P2/dicegame/src/index.js
+++ P2/dicegame/src/index.js
@@ -1,5 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>안녕 리액트!</h1>);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    <h1 id="title">가위바위보</h1>
+    <button class="hand">가위</button>
+    <button class="hand">바위</button>
+    <button class="hand">보</button>
+  </>
+);
+
