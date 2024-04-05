511df9368f83618cc4292db4f35cde67512e5ad2
김보미 <kimbomi2172@naver.com>
2024-04-05 13:12:58 +0900

    ✨ Button 컴포넌트 생성

--- P2/rock-scissor-paper/src/App.js
+++ P2/rock-scissor-paper/src/App.js
@@ -1,9 +1,12 @@
 import HandButton from "./HandButton";
+import Button from "./Button";
 
 function App() {
   const handleClick = (value) => console.log(value);
+  const handleClearClick = (name) => console.log(name)
   return (
     <>
+    <Button onClick={handleClearClick}>처음부터</Button>
       <HandButton value="rock" onClick={handleClick} />
       <HandButton value="scissor" onClick={handleClick} />
       <HandButton value="paper" onClick={handleClick} />
