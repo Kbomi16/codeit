585eec79734bafc8e1c8f6b1c43caebf6260d638
김보미 <kimbomi2172@naver.com>
2024-04-05 13:35:15 +0900

    ✨ state로 주사위 던지기 함수 생성

--- P2/dicegame/src/App.js
+++ P2/dicegame/src/App.js
@@ -1,17 +1,32 @@
-import Dice from './Dice';
-import Button from './Button'
+import Dice from "./Dice";
+import Button from "./Button";
+import { useState } from "react";
+
+function random(n) {
+  return Math.ceil(Math.random() * n);
+}
 
 function App() {
+  const [num, setNum] = useState(1);
+
+  const handleRollClick = () => {
+    const nextNum = random(6);
+    setNum(nextNum);
+  };
+
+  const handleClearClick = () => {
+    setNum(1);
+  };
   return (
     <div>
       <div>
-        <Button text="던지기"/>
-        <Button text="처음부터"/>
+        <Button text="던지기" onClick={handleRollClick} />
+        <Button text="처음부터" onClick={handleClearClick} />
       </div>
-      <Dice color="red" num={2}/>
-      
+      <Dice color="red" num={num} />
     </div>
   );
 }
 
 export default App;
+
