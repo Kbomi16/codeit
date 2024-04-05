7fec18ece778971743dadf0b602768fc9361b7b8
김보미 <kimbomi2172@naver.com>
2024-04-05 12:59:16 +0900

    ✨ Button 컴포넌트 추가

--- P2/dicegame/src/App.js
+++ P2/dicegame/src/App.js
@@ -1,9 +1,15 @@
 import Dice from './Dice';
+import Button from './Button'
 
 function App() {
   return (
     <div>
+      <div>
+        <Button text="던지기"/>
+        <Button text="처음부터"/>
+      </div>
       <Dice color="red" num={2}/>
+      
     </div>
   );
 }
