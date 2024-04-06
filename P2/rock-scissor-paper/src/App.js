bf74e03cc911deb8589b6abb75471e22657ecc47
김보미 <kimbomi2172@naver.com>
2024-04-06 11:49:43 +0900

    ✨ feat: 가위바위보 배점 높이기

--- P2/rock-scissor-paper/src/App.js
+++ P2/rock-scissor-paper/src/App.js
@@ -1,44 +1,76 @@
-import Button from './Button';
-import HandButton from './HandButton';
-import HandIcon from './HandIcon';
-import { compareHand, generateRandomHand } from './utils';
 import { useState } from "react";
+import Button from "./Button";
+import HandButton from "./HandButton";
+import HandIcon from "./HandIcon";
+import { compareHand, generateRandomHand } from "./utils";
+
+const INITIAL_VALUE = "rock";
 
 function getResult(me, other) {
   const comparison = compareHand(me, other);
-  if (comparison > 0) return '승리';
-  if (comparison < 0) return '패배';
-  return '무승부';
+  if (comparison > 0) return "승리";
+  if (comparison < 0) return "패배";
+  return "무승부";
 }
 
 function App() {
-  // hand와 otherHand를 state로 바꿔 주세요
-  const [hand, setHand] = useState('rock')
-  const [otherHand, setotherHand] = useState('rock')
+  const [hand, setHand] = useState(INITIAL_VALUE);
+  const [otherHand, setOtherHand] = useState(INITIAL_VALUE);
+  const [gameHistory, setGameHistory] = useState([]);
+  const [score, setScore] = useState(0);
+  const [otherScore, setOtherScore] = useState(0);
+  const [bet, setBet] = useState(1);
 
   const handleButtonClick = (nextHand) => {
-    const nextOtherHand = generateRandomHand()
-    // hand의 값을 nextHand 로 바꿔 주세요
-    setHand(nextHand)
-    // otherHand의 값을 generateRandomHand()의 리턴 값으로 바꿔주세요
-    setotherHand(nextOtherHand)
+    const nextOtherHand = generateRandomHand();
+    const nextHistoryItem = getResult(nextHand, nextOtherHand);
+    const comparison = compareHand(nextHand, nextOtherHand);
+    setHand(nextHand);
+    setOtherHand(nextOtherHand);
+    setGameHistory([...gameHistory, nextHistoryItem]);
+    if (comparison > 0) setScore(score + bet);
+    if (comparison < 0) setOtherScore(otherScore + bet);
   };
 
   const handleClearClick = () => {
-    // hand와 otherHand의 값을 'rock' 으로 바꿔주세요
-    setHand('rock')
-    setotherHand('rock')
+    setHand(INITIAL_VALUE);
+    setOtherHand(INITIAL_VALUE);
+    setGameHistory([]);
+    setScore(0);
+    setOtherScore(0);
+    setBet(1);
+  };
+  // 원하는 숫자로 배점을 정하면, 게임이 진행될 때마다 이긴 쪽이 해당하는 점수를 얻는 기능
+  const handleBetChange = (e) => {
+    // input의 value 속성을 참조(문자열이기 때문에, 숫자형으로 바꿈) 
+    let num = Number(e.target.value);
+    // if (num > 9) num %= 10;
+    // if (num > 1) num = 1;
+    // num = Math.floor(num);
+    setBet(num);
   };
 
   return (
     <div>
       <Button onClick={handleClearClick}>처음부터</Button>
-      <p>{getResult(hand, otherHand)}</p>
+      <div>
+        {score} : {otherScore}
+      </div>
       <div>
         <HandIcon value={hand} />
         VS
         <HandIcon value={otherHand} />
       </div>
+      <div>
+        <input
+          type="number"
+          value={bet}
+          min={1}
+          max={9}
+          onChange={handleBetChange}
+        ></input>
+      </div>
+      <p>승부 기록: {gameHistory.join(", ")}</p>
       <div>
         <HandButton value="rock" onClick={handleButtonClick} />
         <HandButton value="scissor" onClick={handleButtonClick} />
@@ -49,3 +81,4 @@ function App() {
 }
 
 export default App;
+
