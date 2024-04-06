4011a198d2d8a9e77a3d37aa0ec62521dcf54040
김보미 <kimbomi2172@naver.com>
2024-04-06 12:48:46 +0900

    🔥 refactor: 코드 정리

--- P2/dicegame/src/App.js
+++ P2/dicegame/src/App.js
@@ -8,33 +8,19 @@ function random(n) {
 }
 
 function App(props) {
-  const [num, setNum] = useState(1);
-  const [sum, setSum] = useState(0);
-  const [gameHistory, setGameHistory] = useState([]);
-
-  const [otherNum, setOtherNum] = useState(1);
-  const [otherSum, setOtherSum] = useState(0);
-  const [otherGameHistory, setOtherGameHistory] = useState([]);
+  const [myHistory, setMyHistory] = useState([]);
+  const [otherHistory, setOtherHistory] = useState([]);
 
   const handleRollClick = () => {
-    const nextNum = random(6);
+    const nextMyNum = random(6);
     const nextOtherNum = random(6)
-    setNum(nextNum);
-    setSum(sum + nextNum);
-    // 위 두 줄을 주석처리하면 gameHistory도 안 나옴
-    // 왜? 배열은 기본형이 아닌 참조형임
-    // 배열 자체를 값으로 갖는게 아닌 배열을 가리키고 있는 주솟값을 가리킴
-    // gameHistory.push(nextNum)
-    setGameHistory([...gameHistory, nextNum]);
-    setOtherNum(nextOtherNum)
-    setOtherSum(otherSum + nextOtherNum)
-    setOtherGameHistory([...otherGameHistory, nextOtherNum])
+    setMyHistory([...myHistory, nextMyNum]);
+    setOtherHistory([...otherHistory, nextOtherNum])
   };
 
   const handleClearClick = () => {
-    setNum(1);
-    setSum(0);
-    setGameHistory([]);
+    setMyHistory([]);
+    setOtherHistory([]);
   };
 
   return (
@@ -44,8 +30,8 @@ function App(props) {
         <Button text="처음부터" onClick={handleClearClick} />
       </div>
       <div>
-        <Board name="나" color="blue" num={num} sum={sum} gameHistory={gameHistory}/>
-        <Board name="상대" color="red" num={otherNum} sum={otherSum} gameHistory={otherGameHistory}/>
+        <Board name="나" color="blue" gameHistory={myHistory}/>
+        <Board name="상대" color="red" gameHistory={otherHistory}/>
       </div>
     </>
   );
--- P2/dicegame/src/Board.js
+++ P2/dicegame/src/Board.js
@@ -1,6 +1,11 @@
 import Dice from "./Dice";
 
-function Board({ name, color, num, sum, gameHistory }) {
+function Board({ name, color, gameHistory }) {
+  // gameHistory 배열이 비어있다면(길이가 0이면), 1을 num 변수에 할당
+  const num = gameHistory[gameHistory.length - 1] || 1;
+  // reduce 메서드는 각 요소에 대해 주어진 함수를 실행하고, 이전 값과 현재 값의 합산된 결과를 반환
+  // gameHistory 배열의 모든 요소를 합산하여 sum 변수에 할당
+  const sum = gameHistory.reduce((a, b) => a + b, 0);
   return (
     <div>
       <h2>{name}</h2>
