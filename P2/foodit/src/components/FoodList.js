66767ad6d7d185e6b6c1e1fa31d9a954197351ef
김보미 <kimbomi2172@naver.com>
2024-04-09 15:36:28 +0900

    ✨ foodit: 음식 삭제하기

--- P2/foodit/src/components/FoodList.js
+++ P2/foodit/src/components/FoodList.js
@@ -1,30 +1,34 @@
-import './FoodList.css';
+import "./FoodList.css";
 
 function formatDate(value) {
   const date = new Date(value);
   return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
 }
 
-function FoodListItem({ item }) {
+function FoodListItem({ item, onDelete }) {
+  const handleDeleteClick = () => onDelete(item.id)
   const { imgUrl, title, calorie, content, createdAt } = item;
 
   return (
-    <div className="FoodListItem">
-      <img src={imgUrl} alt={title} />
-      <div>{title}</div>
-      <div>{calorie}</div>
-      <div>{content}</div>
-      <div>{formatDate(createdAt)}</div>
-    </div>
+    <>
+      <div className="FoodListItem">
+        <img src={imgUrl} alt={title} />
+        <div>{title}</div>
+        <div>{calorie}</div>
+        <div>{content}</div>
+        <div>{formatDate(createdAt)}</div>
+      </div>
+      <button onClick={handleDeleteClick}>삭제</button>
+    </>
   );
 }
 
-function FoodList({ items }) {
+function FoodList({ items, onDelete }) {
   return (
     <ul className="FoodList">
       {items.map((item) => (
         <li>
-          <FoodListItem item={item} />
+          <FoodListItem item={item} onDelete={onDelete}/>
         </li>
       ))}
     </ul>
