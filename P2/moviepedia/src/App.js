d1859bd2d8fee389bf628118ad9e0afbab0858e5
김보미 <kimbomi2172@naver.com>
2024-04-09 12:30:02 +0900

    ✨ sort로 정렬 바꾸기

--- P2/moviepedia/src/App.js
+++ P2/moviepedia/src/App.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ReviewList from "./components/ReviewList";
 import items from "./mock.json";
 
 function App() {
+  const [order, setOrder] = useState('createdAt') // 최신순
+  const sortedItems = items.sort((a,b) => b[order] - a[order]) // 평점 높은 순(내림차순)
+
+  const handleNewestClick = () => setOrder('createdAt')
+
+  const handBestClick = () => setOrder('rating')
+
   return (
     <div>
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handBestClick}>베스트순</button>
+      </div>
       <ReviewList items={items} />
     </div>
   );
