191b3d778f5f0c9217d12e5681973fab9c354fd4
김보미 <kimbomi2172@naver.com>
2024-04-09 21:17:49 +0900

    ✨ useEffect로 무한루피 방지

--- P2/moviepedia/src/App.js
+++ P2/moviepedia/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReviewList from "./components/ReviewList";
 import { getReviews } from "./api";
 
@@ -15,10 +15,16 @@ function App() {
     setItems(nextItems);
   };
 
-  const handleLoadClick = async () => {
+  const handleLoad = async () => {
+    // 비동기로 리퀘스를 보냈다가 리스폰스가 도착하면 reviews 변수를 지정하고
     const { reviews } = await getReviews();
-    setItems(reviews)
+    // setItems를 통해 state 변경 -> App 컴포넌트를 다시 렌더링함
+    setItems(reviews);
   };
+
+  useEffect(() => {
+    handleLoad();
+  }, []); // 콜백함수를 맨 처음 렌더링할 때만 실행해서 무한루프를 방지함
 
   return (
     <div>
@@ -27,7 +33,7 @@ function App() {
         <button onClick={handBestClick}>베스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
-      <button onClick={handleLoadClick}>불러오기</button>
+      <button>불러오기</button>
     </div>
   );
 }
