235da333cce74498635bea6041faafafa3fe8789
김보미 <kimbomi2172@naver.com>
2024-04-09 12:06:21 +0900

    ✨ map으로 배열 렌더링하기

---
+++ P2/moviepedia/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReviewList from "./components/ReviewList";
+import items from "./mock.json";
+
+function App() {
+  return (
+    <div>
+      <ReviewList items={items} />
+    </div>
+  );
+}
+
+export default App;
--- P2/moviepedia/src/index.js
+++ P2/moviepedia/src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-function index(props) {
-  return (
-    <div>
-      
-    </div>
-  );
-}
-
-export default index;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <>
+    <App />
+  </>
+);
