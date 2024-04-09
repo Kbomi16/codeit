41fc2d9069f40176659f93a454ca64ac6965c552
김보미 <kimbomi2172@naver.com>
2024-04-09 20:54:17 +0900

    ✨ fetch로 데이터 가져오기

--- P2/moviepedia/src/App.js
+++ P2/moviepedia/src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import ReviewList from "./components/ReviewList";
-import mockItems from "./mock.json";
+import { getReviews } from "./api";
 
 function App() {
-  const [items, setItems] = useState(mockItems)
+  const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt"); // 최신순
   const sortedItems = items.sort((a, b) => b[order] - a[order]); // 평점 높은 순(내림차순)
 
@@ -12,7 +12,12 @@ function App() {
   const handleDelete = (id) => {
     // 현재 순회 중인 요소의 id가 주어진 id와 같지 않은 경우를 체크
     const nextItems = items.filter((item) => item.id !== id);
-    setItems(nextItems)
+    setItems(nextItems);
+  };
+
+  const handleLoadClick = async () => {
+    const { reviews } = await getReviews();
+    setItems(reviews)
   };
 
   return (
@@ -21,7 +26,8 @@ function App() {
         <button onClick={handleNewestClick}>최신순</button>
         <button onClick={handBestClick}>베스트순</button>
       </div>
-      <ReviewList items={sortedItems} onDelete={handleDelete}/>
+      <ReviewList items={sortedItems} onDelete={handleDelete} />
+      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 }
