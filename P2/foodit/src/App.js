e9645b93284346db90f66e23d2a055a564994eab
김보미 <kimbomi2172@naver.com>
2024-04-09 15:14:32 +0900

    ✨ foodit: sort로 정렬 바꾸기

--- P2/foodit/src/App.js
+++ P2/foodit/src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import FoodList from './components/FoodList';
 import items from './mock.json'
 
 function App() {
+  const [order, setOrder] = useState('createdAt')
+  const sortedItems = items.sort((a,b) => b[order] - a[order])
+
+  const handleNewestClick = () => setOrder('createdAt')
+  const handleCalorieClick = () => setOrder('calorie')
+
   return (
     <div>
-      <FoodList items={items} />
+      <button onClick={handleNewestClick}>최신순</button>
+      <button onClick={handleCalorieClick}>칼로리순</button>
+      <FoodList items={sortedItems} />
     </div>
   );
 }
