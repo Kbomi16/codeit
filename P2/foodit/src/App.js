f611838100ff51efe854c64215fd5358e14c00a9
김보미 <kimbomi2172@naver.com>
2024-04-11 15:12:16 +0900

    ✨ 네트워크 로딩오류, 검색

--- P2/foodit/src/App.js
+++ P2/foodit/src/App.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from "react";
 import FoodList from "./components/FoodList";
 import { getFoods } from "./api";
 
-const LIMIT = 10;
-
 function App() {
   const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt");
-  const [cursor, setCursor] = useState(null)
+  const [cursor, setCursor] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadingError, setLoadingError] = useState(null);
+  const [search, setSearch] = useState("");
+
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   const handleNewestClick = () => setOrder("createdAt");
@@ -18,35 +20,59 @@ function App() {
   };
 
   const handleLoad = async (options) => {
-    const { foods, paging: {nextCursor} } = await getFoods(options);
+    let result;
+    try {
+      setIsLoading(true);
+      setLoadingError(null);
+      result = await getFoods(options);
+    } catch (error) {
+      setLoadingError(error);
+      return;
+    } finally {
+      setIsLoading(false);
+    }
+    const {
+      foods,
+      paging: { nextCursor },
+    } = result;
     if (options.cursor) {
       setItems(foods);
     } else {
       setItems((prevItems) => [...prevItems, ...foods]);
     }
-    setCursor(nextCursor)
+    setCursor(nextCursor);
+  };
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    setSearch(e.target["search"].value);
   };
 
   const handleLoadMore = () => {
-    handleLoad({ order, cursor });
+    handleLoad({ order, cursor, search });
   };
 
   useEffect(() => {
-    handleLoad({ order });
-  }, [order]);
+    handleLoad({ order, search });
+  }, [order, search]);
 
   return (
     <>
       <div>
         <button onClick={handleNewestClick}>최신순</button>
         <button onClick={handleCalorieClick}>칼로리순</button>
+        <form onSubmit={handleSearchSubmit}>
+          <input name="search" />
+          <button type="submit">검색</button>
+        </form>
         <FoodList items={sortedItems} onDelete={handleDelete} />
       </div>
       {cursor && (
-        <button onClick={handleLoadMore}>
+        <button disabled={isLoading} onClick={handleLoadMore}>
           더 보기
         </button>
       )}
+      {loadingError?.message && <span>{loadingError.message}</span>}
     </>
   );
 }
