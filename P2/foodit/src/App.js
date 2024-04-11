784438a106f9845917b2e5606861980f8987f290
김보미 <kimbomi2172@naver.com>
2024-04-11 14:40:30 +0900

    ✨ 커서 기반 페이지네이션

--- P2/foodit/src/App.js
+++ P2/foodit/src/App.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from "react";
 import FoodList from "./components/FoodList";
 import { getFoods } from "./api";
 
+const LIMIT = 10;
+
 function App() {
   const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt");
+  const [cursor, setCursor] = useState(null)
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   const handleNewestClick = () => setOrder("createdAt");
@@ -14,21 +17,37 @@ function App() {
     setItems(nextItems);
   };
 
-  const handleLoad = async (orderQuery) => {
-    const { foods } = await getFoods(orderQuery);
-    setItems(foods);
+  const handleLoad = async (options) => {
+    const { foods, paging: {nextCursor} } = await getFoods(options);
+    if (options.cursor) {
+      setItems(foods);
+    } else {
+      setItems((prevItems) => [...prevItems, ...foods]);
+    }
+    setCursor(nextCursor)
+  };
+
+  const handleLoadMore = () => {
+    handleLoad({ order, cursor });
   };
 
   useEffect(() => {
-    handleLoad(order)
-  }, [order])
+    handleLoad({ order });
+  }, [order]);
 
   return (
-    <div>
-      <button onClick={handleNewestClick}>최신순</button>
-      <button onClick={handleCalorieClick}>칼로리순</button>
-      <FoodList items={sortedItems} onDelete={handleDelete} />
-    </div>
+    <>
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleCalorieClick}>칼로리순</button>
+        <FoodList items={sortedItems} onDelete={handleDelete} />
+      </div>
+      {cursor && (
+        <button onClick={handleLoadMore}>
+          더 보기
+        </button>
+      )}
+    </>
   );
 }
 
