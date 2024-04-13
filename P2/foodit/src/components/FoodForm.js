b3862ff712ecce75acf33909f5465084177b87e3
김보미 <kimbomi2172@naver.com>
2024-04-13 11:43:42 +0900

    ✨ feat: 입력 폼 구현

--- P2/foodit/src/components/FoodForm.js
+++ P2/foodit/src/components/FoodForm.js
@@ -1,27 +1,49 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function FoodForm() {
-  const [title, setTitle] = useState('');
-  const [calorie, setCalorie] = useState(0);
-  const [content, setContent] = useState('');
+  const [values, setValues] = useState({
+    title: "",
+    calorie: 0,
+    content: "",
+  });
 
-  
-const handleTitleChange = (e) => setTitle(e.target.value);
+  // const [title, setTitle] = useState('');
+  // const [calorie, setCalorie] = useState(0);
+  // const [content, setContent] = useState('');
 
-const handleCalorieChange = (e) => {
-  const nextCalorie = Number(e.target.value) || 0;
-  setCalorie(nextCalorie);
-};
+  // const handleTitleChange = (e) => setTitle(e.target.value);
 
-const handleContentChange = (e) => setContent(e.target.value);
+  // const handleCalorieChange = (e) => {
+  //   const nextCalorie = Number(e.target.value) || 0;
+  //   setCalorie(nextCalorie);
+  // };
 
+  // const handleContentChange = (e) => setContent(e.target.value);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values)
+  };
+
+  const handleChange = (e) => {
+    const {name, value} = e.target
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value
+    }))
+  };
 
   return (
-    <form>
-      <input name="title" onChange={handleTitleChange}></input>
-      <input type="number" name="calorie" onChange={handleCalorieChange}></input>
-      <input name="content" onChange={handleContentChange}></input>
+    <form onSubmit={handleSubmit}>
+      <input name="title" value={values.title} onChange={handleChange} />
+      <input
+        type="number"
+        name="calorie"
+        value={values.calorie}
+        onChange={handleChange}
+      />
+      <input name="content" value={values.content} onChange={handleChange} />
+      <button type="submit">확인</button>
     </form>
   );
 }
--- P2/moviepedia/src/components/ReviewForm.js
+++ P2/moviepedia/src/components/ReviewForm.js
@@ -2,37 +2,48 @@ import { useState } from 'react';
 import './ReviewForm.css';
 
 function ReviewForm() {
-  const [title, setTitle] = useState('');
-  const [rating, setRating] = useState(0);
-  const [content, setContent] = useState('');
+  const [values, setValues] = useState({
+    title: '',
+    rating:0,
+    content: ''
+  })
 
-  const handleTitleChange = (e) => {
-    setTitle(e.target.value);
-  };
+  // const [title, setTitle] = useState('');
+  // const [rating, setRating] = useState(0);
+  // const [content, setContent] = useState('');
 
-  const handleRatingChange = (e) => {
-    const nextRating = Number(e.target.value);
-    setRating(nextRating);
-  };
+  // const handleTitleChange = (e) => {
+  //   setTitle(e.target.value);
+  // };
 
-  const handleContentChange = (e) => {
-    setContent(e.target.value);
-  };
+  // const handleRatingChange = (e) => {
+  //   const nextRating = Number(e.target.value);
+  //   setRating(nextRating);
+  // };
+
+  // const handleContentChange = (e) => {
+  //   setContent(e.target.value);
+  // };
+
+  const handleChange = (e) => {
+    const {name, value} = e.target
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value // name 값으로 프로퍼티명을 지정하고 해당 값을 지정할 수 있음
+    }))
+  }
 
   const handleSubmit = (e) => {
+    // form기본동작은 submit 버튼을 눌렀을 때 입력 폼 값과 get리퀘스트를 보냄 -> 기본동작 막아야함
     e.preventDefault();
-    console.log({
-      title,
-      rating,
-      content,
-    });
+    console.log(values);
   };
 
   return (
     <form className="ReviewForm" onSubmit={handleSubmit}>
-      <input value={title} onChange={handleTitleChange} />
-      <input type="number" value={rating} onChange={handleRatingChange} />
-      <textarea value={content} onChange={handleContentChange} />
+      <input name="title" value={values.title} onChange={handleChange} />
+      <input type="number" name="rating" value={values.rating} onChange={handleChange} />
+      <textarea name="content" value={values.content} onChange={handleChange} />
       <button type="submit">확인</button>
     </form>
   );
