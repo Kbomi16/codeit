e9c225c967bdd748acb7764f7834133425643965
김보미 <kimbomi2172@naver.com>
2024-04-13 19:58:34 +0900

    ✨ feat: 파일 인풋 초기화

--- P2/moviepedia/src/components/FileInput.js
+++ P2/moviepedia/src/components/FileInput.js
@@ -1,10 +1,34 @@
+import { useEffect, useRef } from "react";
+
 function FileInput({ name, value, onChange }) {
+  // ref를 쓰면 직접 DOM 노드에 접근이 가능함
+  const inputRef = useRef();
+
   const handleChange = (e) => {
     const nextValue = e.target.files[0];
     onChange(name, nextValue);
   };
 
-  return <input type="file" onChange={handleChange} />;
+  // useEffect(() => {
+  //   if (inputRef.current) {
+  //     console.log(inputRef);
+  //   }
+  // }, []);
+
+  const handleClearButton = () => {
+    const inputNode = inputRef.current;
+    if (!inputNode) return;
+
+    inputNode.value = "";
+    onChange(name, null);
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleChange} ref={inputRef} />
+      {value && <button onClick={handleClearButton}>X</button>}
+    </div>
+  );
 }
 
 export default FileInput;
