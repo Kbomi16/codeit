1ab88e32484d3511f8a7c953d47b01f5599525ff
김보미 <kimbomi2172@naver.com>
2024-04-15 15:36:12 +0900

    ✨ feat: 검색 기능

--- P3/S3/codethat/src/pages/CourseListPage.js
+++ P3/S3/codethat/src/pages/CourseListPage.js
@@ -1,17 +1,33 @@
-import { useState } from 'react';
-import ListPage from '../components/ListPage';
-import Warn from '../components/Warn';
-import CourseItem from '../components/CourseItem';
-import { getCourses } from '../api';
-import styles from './CourseListPage.module.css';
-import searchBarStyles from '../components/SearchBar.module.css';
-import searchIcon from '../assets/search.svg';
+import { useState } from "react";
+import ListPage from "../components/ListPage";
+import Warn from "../components/Warn";
+import CourseItem from "../components/CourseItem";
+import { getCourses } from "../api";
+import styles from "./CourseListPage.module.css";
+import searchBarStyles from "../components/SearchBar.module.css";
+import searchIcon from "../assets/search.svg";
+import { useSearchParams } from "react-router-dom";
 
 function CourseListPage() {
-  const [keyword, setKeyword] = useState('');
-  const courses = getCourses();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const initKeyword = searchParams.get("keyword");
+
+  const [keyword, setKeyword] = useState(initKeyword || ""); // 키워드 없으면 빈 문자열
+  const courses = getCourses(initKeyword);
 
   const handleKeywordChange = (e) => setKeyword(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // 키워드 스테이트 값이 없으면 빈 객체 넘기기
+    setSearchParams(
+      keyword
+        ? {
+            keyword,
+          }
+        : {}
+    );
+  };
 
   return (
     <ListPage
@@ -19,7 +35,7 @@ function CourseListPage() {
       title="모든 코스"
       description="자체 제작된 코스들로 기초를 쌓으세요."
     >
-      <form className={searchBarStyles.form}>
+      <form className={searchBarStyles.form} onSubmit={handleSubmit}>
         <input
           name="keyword"
           value={keyword}
@@ -33,7 +49,8 @@ function CourseListPage() {
 
       <p className={styles.count}>총 {courses.length}개 코스</p>
 
-      {courses.length === 0 ? (
+      {/* 검색어가 있을 때 검색된 결과가 없으면 경고 메시지 보여주기 */}
+      {initKeyword && courses.length === 0 ? (
         <Warn
           className={styles.emptyList}
           title="조건에 맞는 코스가 없어요."
@@ -51,3 +68,4 @@ function CourseListPage() {
 }
 
 export default CourseListPage;
+
--- P3/S3/codethat/src/pages/QuestionListPage.js
+++ P3/S3/codethat/src/pages/QuestionListPage.js
@@ -1,14 +1,14 @@
-import { useState } from "react";
-import { getQuestions } from "../api";
-import DateText from "../components/DateText";
-import ListPage from "../components/ListPage";
-import Warn from "../components/Warn";
-import Card from "../components/Card";
-import Avatar from "../components/Avatar";
-import styles from "./QuestionListPage.module.css";
-import searchBarStyles from "../components/SearchBar.module.css";
-import searchIcon from "../assets/search.svg";
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
+import { getQuestions } from '../api';
+import DateText from '../components/DateText';
+import ListPage from '../components/ListPage';
+import Warn from '../components/Warn';
+import Card from '../components/Card';
+import Avatar from '../components/Avatar';
+import styles from './QuestionListPage.module.css';
+import searchBarStyles from '../components/SearchBar.module.css';
+import searchIcon from '../assets/search.svg';
 
 function QuestionItem({ question }) {
   return (
@@ -16,7 +16,6 @@ function QuestionItem({ question }) {
       <div className={styles.info}>
         <p className={styles.title}>
           <Link to={`/questions/${question.id}`}>{question.title}</Link>
-          {question.title}
           {question.answers.length > 0 && (
             <span className={styles.count}>[{question.answers.length}]</span>
           )}
@@ -36,10 +35,17 @@ function QuestionItem({ question }) {
 }
 
 function QuestionListPage() {
-  const [keyword, setKeyword] = useState("");
-  const questions = getQuestions();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const initKeyword = searchParams.get('keyword');
+  const [keyword, setKeyword] = useState(initKeyword || '');
+  const questions = getQuestions(initKeyword);
 
   const handleKeywordChange = (e) => setKeyword(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearchParams(keyword ? { keyword } : {});
+  };
 
   return (
     <ListPage
@@ -47,7 +53,7 @@ function QuestionListPage() {
       title="커뮤니티"
       description="코드댓의 2만 수강생들과 함께 공부해봐요."
     >
-      <form className={searchBarStyles.form}>
+      <form className={searchBarStyles.form} onSubmit={handleSubmit}>
         <input
           name="keyword"
           value={keyword}
@@ -61,7 +67,7 @@ function QuestionListPage() {
 
       <p className={styles.count}>총 {questions.length}개 질문</p>
 
-      {questions.length === 0 ? (
+      {initKeyword && questions.length === 0 ? (
         <Warn
           className={styles.emptyList}
           title="조건에 맞는 질문이 없어요."
@@ -79,4 +85,3 @@ function QuestionListPage() {
 }
 
 export default QuestionListPage;
-
