6022f734a12d018f8c090ff234ae0b7d1d1c5632
김보미 <kimbomi2172@naver.com>
2024-04-15 19:41:37 +0900

    💄 style: 로그인 페이지

--- P3/S4/styledcomponents/src/Input.js
+++ P3/S4/styledcomponents/src/Input.js
@@ -1,20 +1,45 @@
+// import styled from "styled-components";
+
+// const SIZES = {
+//   large: 24,
+//   medium: 20,
+//   small: 16,
+// };
+
+// const Input = styled.input`
+//   border: 2px solid ${({error}) => error ? '#f44336' : '#eee'};
+//   border-radius: ${({ round }) => (round ? `9999px` : `4px`)};
+//   outline: none;
+//   padding: 16px;
+//   font-size: ${({ size }) => SIZES[size] ?? SIZES["medium"]}px;
+
+//   &:focus {
+//     border-color: #7760b4;
+//   }
+// `;
+// export default Input;
 import styled from "styled-components";
 
-const SIZES = {
-  large: 24,
-  medium: 20,
-  small: 16,
-};
-
 const Input = styled.input`
-  border: 2px solid ${({error}) => error ? '#f44336' : '#eee'};
-  border-radius: ${({ round }) => (round ? `9999px` : `4px`)};
+  border: none;
+  border-bottom: 2px solid ${({ error }) => (error ? `#f44336` : `#eeeeee`)};
+  display: block;
+  font-size: 16px;
   outline: none;
-  padding: 16px;
-  font-size: ${({ size }) => SIZES[size] ?? SIZES["medium"]}px;
+  padding: 8px 0;
+  width: 100%;
 
-  &:focus {
-    border-color: #7760b4;
+  ${({ error }) =>
+    !error &&
+    `
+    &:focus {
+      border-bottom: 2px solid #7760b4;
+    }
+`}
+
+  &::placeholder {
+    color: #c4c5cd;
   }
 `;
+
 export default Input;
