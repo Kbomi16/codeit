88d4afd18d3d45bee63dac04128aad6349bb2487
김보미 <kimbomi2172@naver.com>
2024-05-17 12:49:42 +0900

    ✨ 타입

--- P4/typescript-basic/main.ts
+++ P4/typescript-basic/main.ts
@@ -1,1 +1,19 @@
-console.log('하잉')
+let product = {
+  id: 'c001',
+  name: '라이트 윈드 브레이커',
+  price: 129000,
+  sizes: ['M', 'L', 'XL'],
+};
+
+// ...
+
+const newProduct = {
+  id: 'c002',
+  name: '다크 윈드 브레이커',
+  price: 139000,
+  sizes: [90, 95, 100, 105, 100],
+};
+
+// ...
+
+// product = newProduct;
