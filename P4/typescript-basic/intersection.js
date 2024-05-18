79b5a3253c42ca7c971857a7ad3ac6d4ebfb8cf9
김보미 <kimbomi2172@naver.com>
2024-05-18 13:27:38 +0900

    ✨ keyof실습

---
+++ P4/typescript-basic/intersection.js
@@ -0,0 +1,13 @@
+"use strict";
+function printEquipment(equipment) {
+    console.log(`이름: ${equipment.name}`);
+    console.log(`이 장비는 공격력을 ${equipment.attack}, 방어력을 ${equipment.defence} 증가 시킵니다.`);
+}
+const item1 = {
+    id: 'g001',
+    name: '서리불꽃 글러브',
+    price: 100,
+    attack: 5,
+    defence: 42,
+};
+printEquipment(item1);
---
+++ P4/typescript-basic/itemInfo.js
@@ -0,0 +1,15 @@
+"use strict";
+const itemTable = [
+    ['name', '이름'],
+    ['price', '가격'],
+    ['description', '설명'],
+];
+const item = {
+    id: 'h001',
+    name: '힐링 포션',
+    price: 200,
+    description: '마시면 체력을 50 회복한다.',
+};
+for (let [propertyKey, propertyName] of itemTable) {
+    console.log(`${propertyName} | ${item[propertyKey]}`);
+}
