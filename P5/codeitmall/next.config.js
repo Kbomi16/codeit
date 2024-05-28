c201b25c920642831a1da8ddf7f51676433ce59a
김보미 <kimbomi2172@naver.com>
2024-05-28 12:18:25 +0900

    🐛 fix: 리다이렉트 설정

--- P5/codeitmall/next.config.js
+++ P5/codeitmall/next.config.js
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-    async redirects() {
+  // 객체를 배열로 리턴하는 함수
+  async redirects() {
     return [
       {
-        source: '/products/:id',
-        destination: '/items/:id',
-        permanent: true,
+        source: '/products/:id', // 리다이렉트 처리할 주소
+        destination: '/items/:id', // 이동시킬 주소
+        permanent: true, // 웹 브라우저에게 주소가 바꼈다는 사실을 저장하게 함
       },
-    ];
+    ]
   },
 }
 
 module.exports = nextConfig
+
