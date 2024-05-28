2444f749c1e390e8a7ca14ae999ba7cdbabec6ea
김보미 <kimbomi2172@naver.com>
2024-05-28 12:07:59 +0900

    ✨ feat: API 연동

--- P5/codeitmall/pages/search.js
+++ P5/codeitmall/pages/search.js
@@ -1,8 +1,21 @@
+import ProductList from '@/components/ProductList'
 import SearchForm from '@/components/SearchForm'
 import { useRouter } from 'next/router'
 import React from 'react'
 
 export default function search({ initialValue = '' }) {
+  const [products, setProducts] = useState()
+
+  async function getProducts() {
+    const res = await axios.get('/products')
+    const nextProducts = res.data.results
+    setProducts(nextProducts)
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [q])
+
   const router = useRouter()
   const { q } = router.query
   return (
@@ -10,6 +23,7 @@ export default function search({ initialValue = '' }) {
       search
       <SearchForm initialValue={initialValue} />
       <h2>{q} 검색결과</h2>
+      <ProductList />
     </div>
   )
 }
