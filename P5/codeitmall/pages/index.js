8bde847c87f7b77ab562b4365abe1abcc7d4f7f6
김보미 <kimbomi2172@naver.com>
2024-05-28 12:33:52 +0900

    커스텀 App, document

--- P5/codeitmall/pages/index.js
+++ P5/codeitmall/pages/index.js
@@ -1,31 +1,27 @@
-import { useEffect, useState } from 'react';
-import ProductList from '@/components/ProductList';
-import SearchForm from '@/components/SearchForm';
-import axios from '@/lib/axios';
-import styles from '@/styles/Home.module.css';
-import Header from '@/components/Header';
-import Container from '@/components/Container';
+import { useEffect, useState } from 'react'
+import ProductList from '@/components/ProductList'
+import SearchForm from '@/components/SearchForm'
+import axios from '@/lib/axios'
+import styles from '@/styles/Home.module.css'
 
 export default function Home() {
-  const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([])
 
   async function getProducts() {
-    const res = await axios.get('/products');
-    const nextProducts = res.data.results;
-    setProducts(nextProducts);
+    const res = await axios.get('/products')
+    const nextProducts = res.data.results
+    setProducts(nextProducts)
   }
 
   useEffect(() => {
-    getProducts();
-  }, []);
+    getProducts()
+  }, [])
 
   return (
     <>
-      <Header />
-      <Container>
-        <SearchForm />
-        <ProductList className={styles.products} products={products} />
-      </Container>
+      <SearchForm />
+      <ProductList className={styles.products} products={products} />
     </>
   )
 }
+
