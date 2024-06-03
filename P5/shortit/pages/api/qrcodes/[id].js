8746f954fc9f6e1198d68915dcc374d63b972809
김보미 <kimbomi2172@naver.com>
2024-06-03 13:26:28 +0900

    데이터 다루기 실습

--- P5/shortit/pages/api/qrcodes/[id].js
+++ P5/shortit/pages/api/qrcodes/[id].js
@@ -1,19 +1,21 @@
+import QRCode from '@/db/models/QRCode'
+
 export default async function handler(req, res) {
   const { id } = req.query
 
   switch (req.method) {
     case 'GET':
-      res.send(id)
+      const qrcode = await QRCode.findById(id)
+      res.send(qrcode)
       break
 
     case 'PATCH':
-      res.send({
-        ...req.body,
-        id,
-      })
+      const updatedQRCode = await QRCode.findByIdAndUpdate(id, req.body)
+      res.send(updatedQRCode)
       break
 
     case 'DELETE':
+      await QRCode.findByIdAndDelete(id)
       res.status(204).send()
       break
 
