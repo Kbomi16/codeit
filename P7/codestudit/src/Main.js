347acc94184b7f7e5294273248bae996ab7352c2
김보미 <kimbomi2172@naver.com>
2024-07-16 11:23:31 +0900

    ✨ feat: 내 피드 구현하기

--- P7/codestudit/src/Main.js
+++ P7/codestudit/src/Main.js
@@ -1,10 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import App from './components/App';
-import HomePage from './pages/HomePage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import App from './components/App'
+import HomePage from './pages/HomePage'
+import MyFeedPage from './pages/MyFeedPage'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 function Main() {
   return (
@@ -13,12 +14,14 @@ function Main() {
         <Routes>
           <Route path="/" element={<App />}>
             <Route index element={<HomePage />} />
+            <Route path="my-feed" element={<MyFeedPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  );
+  )
 }
 
-export default Main;
+export default Main
+
